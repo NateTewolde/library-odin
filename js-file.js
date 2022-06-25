6f0ab3414574c576bc04b525367a807193a67deb
NateTewolde <tewolde.nate@gmail.com>
2022-06-25 03:55:33 -0700

    Add prototype methods for getting individual book attributes and adding them to the page

--- js-file.js
+++ js-file.js
@@ -1,4 +1,4 @@
-const container = document.querySelector("#container");
+const booksContainer = document.querySelector("#books-container");
 let myLibrary = [];
 
 function Book(title, author, pages, readBook) {
@@ -7,6 +7,22 @@ function Book(title, author, pages, readBook) {
   this.pages = pages;
   this.readBook = readBook;
 }
+
+Book.prototype.titleInfo = function () {
+  return `Title: ${this.title}`;
+};
+
+Book.prototype.authorInfo = function () {
+  return `Author: ${this.author}`;
+};
+
+Book.prototype.pagesInfo = function () {
+  return `Pages: ${this.pages}`;
+};
+
+Book.prototype.readInfo = function () {
+  return `Read? ${this.readBook}`;
+};
 
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readBook}`;
@@ -29,9 +45,28 @@ function addBookToLibrary() {}
 function displayBooks() {
   for (book in myLibrary) {
     const bookDisplay = document.createElement("div");
-    bookDisplay.classList.add("book-display-class");
-    console.log(myLibrary[book].info());
-    bookDisplay.textContent = myLibrary[book].info();
-    container.appendChild(bookDisplay);
+    bookDisplay.classList.add("book-class");
+
+    const titleDisplay = document.createElement("div");
+    titleDisplay.classList.add("title-class");
+    titleDisplay.textContent = myLibrary[book].titleInfo();
+    bookDisplay.appendChild(titleDisplay);
+
+    const authorDisplay = document.createElement("div");
+    authorDisplay.classList.add("author-class");
+    authorDisplay.textContent = myLibrary[book].authorInfo();
+    bookDisplay.appendChild(authorDisplay);
+
+    const pagesDisplay = document.createElement("div");
+    pagesDisplay.classList.add("pages-class");
+    pagesDisplay.textContent = myLibrary[book].pagesInfo();
+    bookDisplay.appendChild(pagesDisplay);
+
+    const readDisplay = document.createElement("div");
+    readDisplay.classList.add("read-class");
+    readDisplay.textContent = myLibrary[book].readInfo();
+    bookDisplay.appendChild(readDisplay);
+
+    booksContainer.appendChild(bookDisplay);
   }
 }
